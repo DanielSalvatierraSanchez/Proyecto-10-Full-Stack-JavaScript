e3bc71e9e8aa54c6336883616515724ee8b2869c
dsalvatierra <dss250583@gmail.com>
2024-09-22 09:43:06 +0200

    FIX index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const { connectDB } = require('./src/config/db')
 
 const app = express()
+connectDB()
 
 app.use(cors())
 app.use(express.json())
