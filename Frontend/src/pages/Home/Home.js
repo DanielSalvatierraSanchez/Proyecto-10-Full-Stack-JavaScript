3d0c575ed17bf0753d614bee1613e4c362615c4b
dsalvatierra <dss250583@gmail.com>
2024-10-23 16:57:03 +0200

    FEAT page Home

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,7 +1,34 @@
+import { Button } from "../../components/Button/Button";
+import { LoginForm } from "../../components/LoginForm/LoginForm";
+import { RegisterForm } from "../../components/RegisterForm/RegisterForm";
 import { createPage } from "../../functions/createPage";
+import { Login } from "../Login/login";
 import "./Home.css";
 
 export const Home = () => {
     const div = createPage("Home");
-    div.innerHTML = "<h1>Esto es la Home</h1>";
+    div.innerHTML = "<h1>Bienvenido a APPADEL</h1>";
+    const homeContainer = document.createElement("div");
+    homeContainer.classList.add("home-container");
+    homeContainer.innerHTML = `
+    <h1>Bienvenido a APPADEL</h1>
+    <br/>
+    <p>La APP de gestión de partidos de padel donde podrás crear y encontrar partidos donde poder apuntarte a ellos.</p>
+    `;
+    // homeContainer.textContent = "Home";
+    div.append(homeContainer);
+    Button({
+        text: "Login / Registro",
+        fnc: () => {
+            showForm = !showForm;
+            showForm ? LoginForm(form) : RegisterForm(form);
+        }
+    });
+    // Button({
+    //     text: "Login / Registro",
+    //     fnc: () => {
+    //         showForm = !showForm;
+    //         showForm ? LoginForm(form) : RegisterForm(form);
+    //     }
+    // });
 };
